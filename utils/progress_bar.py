a647e86bfd852fca347564cd7eb71e342001c8a3
Hadleigh-Schwartz <hadleigh.schwartz@gmail.com>
2025-08-06 11:03:39 -0400

    remove files from here and put in acoustic_embedding repo folder

--- utils/progress_bar.py
+++
@@ -1,5 +0,0 @@
-from pytorch_lightning.callbacks.progress.rich_progress import RichProgressBar
-from pytorch_lightning.callbacks.progress.rich_progress import RichProgressBarTheme
-
-def getProgressBar(config):
-    return RichProgressBar(**config['RichProgressBar'],theme=RichProgressBarTheme(**config['RichProgressBarTheme']))
